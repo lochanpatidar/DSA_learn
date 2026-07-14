b09054faa9c790c765c6911296554525fdc6adc1
lochanpatidar <lochanpatidar6@gmail.com>
2026-07-14 11:54:38 -0700

    arraydelete

--- Array/ArrayDelete.java
+++ Array/ArrayDelete.java
@@ -3,6 +3,12 @@ package Array;
 public class ArrayDelete {
     public static void main(String[] args) {
         int[] arr={3,6,4,7,9};
-        
+        int pos=2;
+        for(int i=pos; i<arr.length-1; i++){
+            arr[i]=arr[i+1];
+        }
+        for(int i=0; i<arr.length-1; i++){
+            System.err.println(arr[i]+" ");
+        }
     }
 }
