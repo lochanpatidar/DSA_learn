d44a7aeb964fb0f72759a96bccf5e8a66b34ca5c
lochanpatidar <lochanpatidar6@gmail.com>
2026-07-15 09:36:06 -0700

    find

--- Array/FindMissing.java
+++ Array/FindMissing.java
@@ -1,8 +1,15 @@
 package Array;
-
+import java.util.Scanner;
 public class FindMissing {
     public static void main(String[] args){
-        int[] arr={1,2,3,5,6,7};
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter array size:");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        System.out.println("enter the value:");
+        for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
         for(int i=0; i<arr.length; i++){
             if (arr[i+1] != arr[i]+1){
                 System.out.println("missing value:"+(arr[i]+1));
