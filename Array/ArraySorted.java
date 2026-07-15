7d26b52835a2a49f5ee994b4f09f00f964c03f5a
lochanpatidar <lochanpatidar6@gmail.com>
2026-07-15 08:43:02 -0700

    sorted

--- Array/ArraySorted.java
+++ Array/ArraySorted.java
@@ -1,8 +1,18 @@
 package Array;
 
+import java.util.Scanner;
+
 public class ArraySorted {
     public static void main(String[] args) {
-        int[] arr={10,5,15,20,6};
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the size of array:");
+        int n= sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+        for(int i=0; i<n; i++){
+         arr[i]=sc.nextInt();
+        }
         int sort=0;
         for(int i=0; i<arr.length; i++){
             if(arr[i]>arr[i+1]){
