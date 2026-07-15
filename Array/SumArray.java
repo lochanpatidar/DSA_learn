c37aca6de7c44b884192aadbad8e0dffd060194b
lochanpatidar <lochanpatidar6@gmail.com>
2026-07-15 08:02:26 -0700

    sum of array

--- Array/SumArray.java
+++ Array/SumArray.java
@@ -2,7 +2,12 @@ package Array;
 
 public class SumArray {
     public static void main(String[] args) {
-        
+        int[] arr={1,2,4,6,8,5,7};
+        int sum=0;
+        for (int i=0; i<arr.length; i++){
+            sum+=arr[i];
+        }
+        System.out.println("sum of array:" +sum);
     }
     
 }
