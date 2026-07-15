eb898078943cb8d5665be78cfac48ea757b4c6c4
lochanpatidar <lochanpatidar6@gmail.com>
2026-07-15 00:11:45 -0700

    new program

---
+++ Array/LargestElement.java
@@ -0,0 +1,17 @@
+package Array;
+public class LargestElement {
+    public static void main(String[] args) {
+       int[] arr={3,5,9,1,7,2};
+       int max=arr[0];
+       for(int i=0; i<arr.length; i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+       }
+        System.out.println("max value:"+max);
+
+         
+
+    }
+    
+}
