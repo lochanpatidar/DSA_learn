a432b81c440064573d5a95ffe21228747f7b5644
lochanpatidar <lochanpatidar6@gmail.com>
2026-07-15 07:33:41 -0700

    reverse

---
+++ Array/Reverse.java
@@ -0,0 +1,16 @@
+package Array;
+public class Reverse {
+    public static void main(String[] args) {
+        int[] arr = {10, 30, 35, 40, 45};
+        int j = arr.length - 1;
+        for (int i = 0; i < j; i++) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            j--;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println( arr[i]+ " ");
+        }
+    }
+}
