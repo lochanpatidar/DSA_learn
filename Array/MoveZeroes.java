1b69d936a7797f6dbe5adb25f77127704e3fc58c
lochanpatidar <lochanpatidar6@gmail.com>
2026-07-16 11:03:40 -0700

    add

--- Array/MoveZeroes.java
+++ Array/MoveZeroes.java
@@ -2,7 +2,7 @@ package Array;
 
 public class MoveZeroes {
     public static void main(String[] args) {
-        int[] arr={1,0,2,0,5,6};
+        int[] arr={1,0,2,0,5,6,0};//move Zeroes to the end
         int count=0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] != 0) {
