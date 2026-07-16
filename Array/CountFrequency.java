4f3a979a2608399cf24a1785309d01bd3f7529e9
lochanpatidar <lochanpatidar6@gmail.com>
2026-07-16 07:03:11 -0700

    add

---
+++ Array/CountFrequency.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class CountFrequency {
+    public static void main(String[] args) {
+        int[] arr={10,20,20,30,40,10};
+        for(int i=0; i<a)
+    }
+    
+    
+}
