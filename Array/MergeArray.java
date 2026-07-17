7eb6f56ebd6774491aebc637efbbbb89e9b21803
lochanpatidar <lochanpatidar6@gmail.com>
2026-07-17 10:05:57 -0700

    add

--- Array/MergeArray.java
+++ Array/MergeArray.java
@@ -5,7 +5,15 @@ public class MergeArray {
         int[] arr1={2,4,6,8};
         int[] arr2={10,12,14,16};
         int[] arr3=new int[arr1.length+arr2.length];
-        
+        for(int i=0; i<arr1.length; i++){
+            arr3[i]=arr1[i];
+        }
+        for(int i=0; i<arr2.length; i++){
+            arr3[arr1.length+i]=arr2[i];
+        }
+        for (int i=0; i<arr3.length; i++){
+            System.out.print(arr3[i]+ " " );
+        }
     }
     
 }
