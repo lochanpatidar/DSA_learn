07cb6ccae104f9dc4a3719591deb5032683184d8
lochanpatidar <lochanpatidar6@gmail.com>
2026-07-17 09:54:45 -0700

    add

---
+++ Array/MergeArray.java
@@ -0,0 +1,11 @@
+package Array;
+
+public class MergeArray {
+    public static void main(String[] args) {
+        int[] arr1={2,4,6,8};
+        int[] arr2={10,12,14,16};
+        int[] arr3=new int[arr1.length+arr2.length];
+        
+    }
+    
+}
