690584b25ef743e2a65092ffa371a3fdb6d5c4fb
lochanpatidar <lochanpatidar6@gmail.com>
2026-07-18 12:25:24 -0700

    insert

--- Array/MergeSort.java
+++ Array/MergeSort.java
@@ -1,5 +1,46 @@
 package Array;
+import java.util.Arrays;
 
 public class MergeSort {
-    
+    static void mergeSort(int[] arr) {
+        if (arr.length == 1)
+            return;
+        int mid = arr.length / 2;
+        int[] left = Arrays.copyOfRange(arr, 0, mid);
+        int[] right = Arrays.copyOfRange(arr, mid, arr.length);
+
+        mergeSort(left);
+        mergeSort(right);
+        merge(arr, left, right);
+    }
+    static void merge(int[] arr, int[] left, int[] right) {
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j]) {
+                arr[k] = left[i];
+                i++;
+            } else {
+                arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < left.length) {
+            arr[k] = left[i];
+            i++;
+            k++;
+        }
+        while (j < right.length) {
+            arr[k] = right[j];
+            j++;
+            k++;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {8, 3, 5, 4, 7, 6, 1, 2};
+        mergeSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
 }
