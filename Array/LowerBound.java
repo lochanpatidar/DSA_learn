11bbff938577f877fda24ec5679b69d0df2dd692
lochanpatidar <lochanpatidar6@gmail.com>
2026-07-20 10:26:24 -0700

    add new

---
+++ Array/LowerBound.java
@@ -0,0 +1,21 @@
+package Array;
+public class LowerBound {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 4, 5, 6};
+        int target = 4;
+        int low = 0;
+        int high = arr.length - 1;
+        int ans = arr.length;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] >= target) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        System.out.println("Lower Bound Index = " + ans);
+    }
+}
+
