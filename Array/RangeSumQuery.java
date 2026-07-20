ed18841e985a4fd05b0571b999e2e1787a6739bd
lochanpatidar <lochanpatidar6@gmail.com>
2026-07-20 10:55:50 -0700

    sum query

--- Array/RangeSumQuery.java
+++ Array/RangeSumQuery.java
@@ -2,7 +2,7 @@ package Array;
 
 public class RangeSumQuery {
     public static void main(String[] args) {
-        int[] arr = {2, 4, 6, 8, 10};
+        int[] arr = {1,3,7,8,5,9,4};
         int[] prefix = new int[arr.length];
         prefix[0] = arr[0];
         for (int i = 1; i < arr.length; i++) {
