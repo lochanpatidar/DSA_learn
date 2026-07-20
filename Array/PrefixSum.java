162aa42a5d1a67ac29e8f98361bd48e46578af86
lochanpatidar <lochanpatidar6@gmail.com>
2026-07-20 10:45:44 -0700

    add prefix

---
+++ Array/PrefixSum.java
@@ -0,0 +1,16 @@
+package Array;
+
+public class PrefixSum {
+    public static void main(String[] args) {
+        int[] arr={2,3,5,7,8,9};
+        int[] prefix = new int[arr.length];
+        prefix[0]=arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            prefix[i] = prefix[i - 1] + arr[i];
+        }
+        for (int i = 0; i < prefix.length; i++) {
+            System.out.print(prefix[i] + " ");
+        }
+
+    }
+}
