a8b6f2cdbe37c885a864e5ec558dcddbdea84591
lochanpatidar <lochanpatidar6@gmail.com>
2026-07-21 12:00:00 -0700

    running

--- Array/FindDuplicate.java
+++ Array/FindDuplicate.java
@@ -2,7 +2,7 @@ package Array;
 
 public class FindDuplicate {
     public static void main(String[] args) {
-        int[] arr={10,20,30,10,40,20};
+        int[] arr={10,20,30,43,40,20};
         for(int i=0; i<arr.length; i++){
           for(int j=i+1; j<arr.length; j++){
             if(arr[i]==arr[j]){
