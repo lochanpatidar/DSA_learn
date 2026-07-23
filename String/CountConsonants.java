9896242d7a3b944b7a3d8e75277793f6af6ec063
lochanpatidar <lochanpatidar6@gmail.com>
2026-07-22 22:26:22 -0700

    string

---
+++ String/CountConsonants.java
@@ -0,0 +1,24 @@
+package String;
+import java.util.Scanner;
+public class CountConsonants {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a String: ");
+        String str = sc.nextLine();
+        int count = 0;
+        System.err.print("consonat word");
+        for (int i = 0; i < str.length(); i++) {
+            char ch = Character.toLowerCase(str.charAt(i));
+            if (ch >= 'a' && ch <= 'z') {
+                if (ch != 'a' && ch != 'e' && ch != 'i' && ch != 'o' && ch != 'u') {
+                    System.err.print(ch+ "  ");
+                    count++;
+                    
+                }
+            }
+        }
+        System.out.println("Number of Consonants: " + count);
+        sc.close();
+    }
+}
+
