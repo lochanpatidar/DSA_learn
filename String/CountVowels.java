f677f241dc9db0b3bd72b194de05a63d4c68d481
lochanpatidar <lochanpatidar6@gmail.com>
2026-07-22 22:11:51 -0700

    string

---
+++ String/CountVowels.java
@@ -0,0 +1,19 @@
+package String;
+ import java.util.Scanner;
+public class CountVowels {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a String: ");
+        String str = sc.nextLine();
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char ch = Character.toLowerCase(str.charAt(i));
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                count++;
+            }
+        }
+        System.out.println("Number of Vowels: " + count);
+        sc.close();
+    }
+}
+
