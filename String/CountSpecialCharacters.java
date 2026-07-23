9a5ae4dd13a12c35be91f787f24780eb84cfd71b
lochanpatidar <lochanpatidar6@gmail.com>
2026-07-22 22:53:55 -0700

    char

--- String/CountSpecialCharacters.java
+++ String/CountSpecialCharacters.java
@@ -6,9 +6,11 @@ public class CountSpecialCharacters {
         System.out.print("Enter a String: ");
         String str = sc.nextLine();
         int count = 0;
+        System.out.println("special character: ");
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
             if (!Character.isLetterOrDigit(ch) && !Character.isWhitespace(ch)) {
+                System.err.println(ch+ "");
                 count++;
             }
         }
