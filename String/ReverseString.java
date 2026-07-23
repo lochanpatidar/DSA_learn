2076180d46610e3c2cb4abe9a9390bb13208ea58
lochanpatidar <lochanpatidar6@gmail.com>
2026-07-22 21:54:14 -0700

    string

---
+++ String/ReverseString.java
@@ -0,0 +1,14 @@
+package String;
+import java.util.Scanner;
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a String: ");
+        String str = sc.nextLine();
+        for (int i = str.length() - 1; i >= 0; i--) {
+            System.out.print(str.charAt(i));
+        }
+        sc.close();
+    }
+}
+
