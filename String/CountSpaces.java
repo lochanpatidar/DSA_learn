5b86f62f2bfd860cd6e661e21fad174736308e22
lochanpatidar <lochanpatidar6@gmail.com>
2026-07-22 23:00:44 -0700

    space

---
+++ String/CountSpaces.java
@@ -0,0 +1,18 @@
+package String;
+ import java.util.Scanner;
+public class CountSpaces {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a String: ");
+        String str = sc.nextLine();
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' ') {
+                count++;
+            }
+        }
+        System.out.println("Number of Spaces: " + count);
+        sc.close();
+    }
+}
+
