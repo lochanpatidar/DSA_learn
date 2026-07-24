fa79081febf4c1e7807cd53de7e4455921701128
lochanpatidar <lochanpatidar6@gmail.com>
2026-07-24 00:09:22 -0700

    convert

--- String/LowerCase.java
+++ String/LowerCase.java
@@ -1,5 +1,11 @@
 package String;
-
+import java.util.Scanner;
 public class LowerCase {
-    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a string: ");
+        String str=sc.nextLine();
+        String lower=str.toLowerCase();
+        System.out.println("upper to lower: " +lower);
+    }
 }
