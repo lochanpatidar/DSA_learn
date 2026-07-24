b8605d971869a47755e9c1951282cf9ac353b251
lochanpatidar <lochanpatidar6@gmail.com>
2026-07-24 00:23:56 -0700

    Toggle case

---
+++ String/ToggleCase.java
@@ -0,0 +1,25 @@
+package String;
+
+import java.util.Scanner;
+
+public class ToggleCase {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a string: ");
+        String str=sc.nextLine();
+        String result="";
+        for(int i=0;i<str.length(); i++){
+            char ch=str.charAt(i);
+            if (Character.isUpperCase(ch)) {
+                result += Character.toLowerCase(ch);
+            } 
+            else if (Character.isLowerCase(ch)) {
+                result += Character.toUpperCase(ch);
+            } 
+            else {
+                result += ch;
+            }
+        }
+        System.out.println("togglecase: "+result);
+    }
+}
