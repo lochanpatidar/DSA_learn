72ea7fe53eb00d4541d2b6c56138dc5474f3021b
lochanpatidar <lochanpatidar6@gmail.com>
2026-07-25 21:47:14 -0700

    RotationString

---
+++ String/RotationString.java
@@ -0,0 +1,18 @@
+package String;
+    import java.util.Scanner;
+public class RotationString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first String: ");
+        String str1 = sc.nextLine();
+        System.out.print("Enter second String: ");
+        String str2 = sc.nextLine();
+        if (str1.length() == str2.length() &&
+                (str1 + str1).contains(str2)) {
+            System.out.println("Rotation");
+        } else {
+            System.out.println("Not Rotation");
+        }
+        sc.close();
+    }
+}
