cbce0324bcc8f110f96f36b4397114236e3199b0
lochanpatidar <lochanpatidar6@gmail.com>
2026-07-25 22:26:24 -0700

    ConcatenateStrings

---
+++ String/ConcatenateStrings.java
@@ -0,0 +1,14 @@
+package String;
+    import java.util.Scanner;
+public class ConcatenateStrings {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First String: ");
+        String str1 = sc.nextLine();
+        System.out.print("Enter Second String: ");
+        String str2 = sc.nextLine();
+        String result = str1 + str2;
+        System.out.println("Concatenated String: " + result);
+        sc.close();
+    }
+}
