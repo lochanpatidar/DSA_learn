3e9b8aa43987882274e2479a1397c99184ad70d9
lochanpatidar <lochanpatidar6@gmail.com>
2026-07-25 22:52:42 -0700

    TrimString

--- String/SplitString.java
+++ String/SplitString.java
@@ -3,7 +3,7 @@ package String;
 public class SplitString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Sentence: ");
+        System.out.print("Enter a Sentence: ");
         String str = sc.nextLine();
         String[] words = str.split(" ");
         System.out.println("Words are:");
