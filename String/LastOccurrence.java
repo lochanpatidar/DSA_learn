fd839716ca55023b0b7806d5b44122482e0fd992
lochanpatidar <lochanpatidar6@gmail.com>
2026-07-25 23:03:48 -0700

    LastOccurrence

---
+++ String/LastOccurrence.java
@@ -0,0 +1,19 @@
+package String;
+    import java.util.Scanner;
+public class LastOccurrence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter String: ");
+        String str = sc.nextLine();
+        System.out.print("Enter Character: ");
+        char ch = sc.next().charAt(0);
+        int index = -1;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ch) {
+                index = i;
+            }
+        }
+        System.out.println("Last Occurrence: " + index);
+        sc.close();
+    }
+}
