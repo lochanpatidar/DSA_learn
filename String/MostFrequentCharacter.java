641b20e732ad9c189133705d5c91b1668fa85d91
lochanpatidar <lochanpatidar6@gmail.com>
2026-07-28 23:50:35 -0700

    MostFrequentCharacter

---
+++ String/MostFrequentCharacter.java
@@ -0,0 +1,26 @@
+package String;
+import java.util.Scanner;
+public class MostFrequentCharacter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter String: ");
+        String str = sc.nextLine();
+        char result = ' ';
+        int max = 0;
+        for (int i = 0; i < str.length(); i++) {
+            int count = 0;
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(i) == str.charAt(j))
+                    count++;
+
+            }
+            if (count > max) {
+
+                max = count;
+                result = str.charAt(i);
+            }
+        }
+        System.out.println(result + " = " + max);
+        sc.close();
+    }
+}
