0e3e66091ae9e355cf4a4bc4f4fc3a9bf0c69191
lochanpatidar <lochanpatidar6@gmail.com>
2026-07-30 23:49:59 -0700

    PalindromeRecursion

---
+++ String/PalindromeRecursion.java
@@ -0,0 +1,21 @@
+package String;
+    import java.util.Scanner;
+public class PalindromeRecursion {
+    static boolean palindrome(String str, int start, int end) {
+        if (start >= end)
+            return true;
+        if (str.charAt(start) != str.charAt(end))
+            return false;
+        return palindrome(str, start + 1, end - 1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter String: ");
+        String str = sc.nextLine();
+        if (palindrome(str, 0, str.length() - 1))
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+        sc.close();
+    }
+}
