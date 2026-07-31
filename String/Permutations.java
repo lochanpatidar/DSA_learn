075b590a2ae2cee8d5886dfc867a45269c302cd8
lochanpatidar <lochanpatidar6@gmail.com>
2026-07-31 00:06:30 -0700

    Permutations

--- String/Permutations.java
+++ String/Permutations.java
@@ -1,11 +1,10 @@
 package String;
-    import java.util.Scanner;
+import java.util.Scanner;
 public class Permutations {
     static void permutation(String str, String ans) {
         if (str.length() == 0) {
             System.out.println(ans);
             return;
-
         }
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
