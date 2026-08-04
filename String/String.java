56bf921977ddf278dc56cc232beca1a288e90dcf
lochanpatidar <lochanpatidar6@gmail.com>
2026-08-03 21:51:54 -0700

    add

--- String/String.java
+++ String/String.java
@@ -2,7 +2,9 @@ package String;
 
 public class String {
     public static void main(String[] args) {
-        String str="lochan";
-        System.out.println(str+ " ");
+        String str1="lochan";
+        String str2="patidar"
+        String str3=str1+str2;
+        System.out.println(str3+ " ");
     }
 }
