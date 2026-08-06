374fc8b8a22b6033be5b26ac8d3d030d81bb3dc3
lochanpatidar <lochanpatidar6@gmail.com>
2026-08-05 23:26:19 -0700

    string add

--- String/String1.java
+++ String/String1.java
@@ -3,8 +3,11 @@ package String;
 import java.util.Scanner;
 
 public class String1 {
+      public static void main(String[] args) {
      Scanner var1 = new Scanner(System.in);
       System.out.print("Enter a String: ");
       String str=var1.nextLine();
       System.out.print(str+ " ");
+ }
 }
+
