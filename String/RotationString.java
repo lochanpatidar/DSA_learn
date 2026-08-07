b48dd0575684ce6761f7c86e762e304a9e414866
lochanpatidar <lochanpatidar6@gmail.com>
2026-08-07 03:13:05 -0700

    string add

--- String/RotationString.java
+++ String/RotationString.java
@@ -5,7 +5,7 @@ public class RotationString {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter first String: ");
         String str1 = sc.nextLine();
-        System.out.print("Enter second String: ");
+        System.out.print("Enter second of String: ");
         String str2 = sc.nextLine();
         if (str1.length() == str2.length() &&
                 (str1 + str1).contains(str2)) {
---
+++ String/String2.java
@@ -0,0 +1,7 @@
+package String;
+public class String2 {
+
+    public static void main(String[] args) {
+        
+    }
+}
