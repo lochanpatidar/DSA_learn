747a1dd3c0d3c2933d5287a4fdb217274337a7cd
lochanpatidar <lochanpatidar6@gmail.com>
2026-08-09 11:56:56 -0700

    stack

--- Stack/Push.java
+++ Stack/Push.java
@@ -1,24 +1,22 @@
 package Stack;
 
 public class Push {
-    static void push(int value) {
+    int[] arr = new int[5];
+    int top = -1;
+    // Push operation
+    void push(int value) {
         if (top == 4) {
             System.out.println("Stack Overflow");
         } else {
             top++;
-            stack[top] = value;
+            arr[top] = value;
             System.out.println(value + " pushed");
         }
     }
-    static void display() {
-        System.out.print("Stack: ");
-        for (int i = top; i >= 0; i--) {
-            System.out.print(stack[i] + " ");
-        }
-        System.out.println();
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(10);
+        s.push(20);
+        s.push(30);
     }
-}     
- public static  void main(String[] args){
-    push(20);
-    display();
- }
+}
