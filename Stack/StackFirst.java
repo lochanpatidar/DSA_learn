806ad50a227e5c67bf5ffeb83baa8f42905b9000
lochanpatidar <lochanpatidar6@gmail.com>
2026-08-08 23:25:36 -0700

    StackFirst

---
+++ Stack/StackFirst.java
@@ -0,0 +1,39 @@
+package Stack;
+
+public class StackFirst {
+    static int[] stack = new int[5];
+    static int top = -1;
+    // Push operation
+    static void push(int value) {
+        if (top == 4) {
+            System.out.println("Stack Overflow");
+        } else {
+            top++;
+            stack[top] = value;
+            System.out.println(value + " pushed");
+        }
+    }
+    static void pop() {
+        if (top == -1) {
+            System.out.println("Stack Underflow");
+        } else {
+            System.out.println(stack[top] + " popped");
+            top--;
+        }
+    }
+    static void display() {
+        System.out.print("Stack: ");
+        for (int i = top; i >= 0; i--) {
+            System.out.print(stack[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        push(10);
+        push(20);
+        push(30);
+        display();
+        pop();
+        display();
+    }
+}
