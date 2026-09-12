c131591d244b89f24d300506eed2aa3523bc3849
lochanpatidar <lochanpatidar6@gmail.com>
2026-09-11 19:20:55 -0700

    add new stack

---
+++ Stack/Stack1.java
@@ -0,0 +1,12 @@
+package Stack;
+import java.util.Stack;
+public class Stack1 {
+   public static void main(String[] args) {
+     Stack<String> st =new Stack<>();
+      st.push("riya");
+      st.push("priya");
+      st.push("jiya");
+      System.out.println(st.size());
+      
+   } 
+}
